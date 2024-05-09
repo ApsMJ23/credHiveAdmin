12c1d399f8f7ae7048e1ae3af60428c2e3e3e567
Apurva Singh <apurvalion@gmail.com>
2024-05-09 20:43:36 +0530

    Build Fixes

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -2,7 +2,6 @@ import {Outlet} from "react-router-dom";
 import useWindowDimensions from "../common/Hooks/useWindowDimensions.tsx";
 import Sidebar from "../common/Components/Sidebar/Sidebar.tsx";
 import styles from './Routes.module.scss';
-import { useEffect } from "react";
 
 const PrivateRoutes = () => {
     const {height,width} = useWindowDimensions();
